c0ca2109a48193cf5aa3f44cd69639338bc79eff
Sheena Artrip <sheena.artrip@gmail.com>
2016-08-27 06:15:39 -0400

    v3 - Move ChannelData to channel file

--- channel.go
+++ channel.go
@@ -67,6 +67,18 @@ type Channel interface {
 	StopSilence(id string) error
 }
 
+// ChannelData is the data for a specific channel
+type ChannelData struct {
+	ID           string      `json:"id"`    // Unique id for this channel (same as for AMI)
+	Name         string      `json:"name"`  // Name of this channel (tech/name-id format)
+	State        string      `json:"state"` // State of the channel
+	Accountcode  string      `json:"accountcode"`
+	Caller       CallerID    `json:"caller"`    // CallerId of the calling endpoint
+	Connected    CallerID    `json:"connected"` // CallerId of the connected line
+	Creationtime DateTime    `json:"creationtime"`
+	Dialplan     DialplanCEP `json:"dialplan"` // Current location in the dialplan
+}
+
 // NewChannelHandle returns a handle to the given ARI channel
 func NewChannelHandle(id string, c Channel) *ChannelHandle {
 	return &ChannelHandle{
