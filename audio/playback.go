8f5339130c2c6b0c9f4699ef6bf499ce74b644e8
Sheena Artrip <sheena.artrip@gmail.com>
2016-09-06 04:40:45 -0400

    v3 - audio - allow Nil playback objects to be operable

--- audio/playback.go
+++ audio/playback.go
@@ -14,26 +14,41 @@ type Playback struct {
 
 // Handle returns the ARI reference to the playback object
 func (p *Playback) Handle() *ari.PlaybackHandle {
+	if p == nil {
+		return nil
+	}
 	return p.handle
 }
 
 // StartCh returns the channel that is closed when the playback has started
 func (p *Playback) StartCh() <-chan struct{} {
+	if p == nil {
+		return nil
+	}
 	return p.startCh
 }
 
 // StopCh returns the channel that is closed when the playback has stopped
 func (p *Playback) StopCh() <-chan struct{} {
+	if p == nil {
+		return nil
+	}
 	return p.stopCh
 }
 
 // Err returns any accumulated errors during playback
 func (p *Playback) Err() error {
+	if p == nil {
+		return nil
+	}
 	return p.err
 }
 
 // Cancel cancels the playback
 func (p *Playback) Cancel() {
+	if p == nil {
+		return
+	}
 	if p.quitCh != nil {
 		close(p.quitCh)
 	}
