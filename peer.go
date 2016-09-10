aa81d78935466637c7e49060686bcf0436bc4f0c
system_user <sallsup@gmail.com>
2016-09-09 21:08:31 -0400

    v3 - Pull in Peer & ContactInfo

---
+++ peer.go
@@ -0,0 +1,10 @@
+package ari
+
+// Peer describes a remote peer that communicates with Asterisk
+type Peer struct {
+	Address string   `json:"address,omitempty"` // IP Address of the peer
+	Cause   string   `json:"cause,omitempty"`   // Reason associated with change in peer_status
+	Status  string   `json:"peer_status"`       // The current state of the peer
+	Port    string   `json:"port,omitempty"`    // The port of the peer
+	Time    DateTime `json:"time,omitempty"`    // The last known time the peer was contacted
+}
