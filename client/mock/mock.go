678bbe53cb32158658f61af5e5d65ce02a872b42
Sheena Artrip <sheena.artrip@gmail.com>
2016-09-13 06:44:37 -0400

    v3 - mock - Add variables mock

--- client/mock/mock.go
+++ client/mock/mock.go
@@ -2,3 +2,4 @@ package mock
 
 //go:generate mockgen -package mock -destination application.go github.com/CyCoreSystems/ari Application
 //go:generate mockgen -package mock -destination asterisk.go github.com/CyCoreSystems/ari Asterisk
+//go:generate mockgen -package mock -destination variables.go github.com/CyCoreSystems/ari Variables
