6a8f187aeb937ab60495771252016bd40c89ab35
Sheena Artrip <sheena.artrip@gmail.com>
2016-09-14 02:53:13 -0400

    server - natsgw - added named handler. Ported asterisk to use named handler

--- server/natsgw/asterisk.go
+++ server/natsgw/asterisk.go
@@ -4,34 +4,33 @@ import "encoding/json"
 
 func (srv *Server) asterisk() {
 
-	srv.subscribe("ari.asterisk.reload.>", func(subj string, _ []byte, reply Reply) {
-		name := subj[len("ari.asterisk.reload."):]
-		err := srv.upstream.Asterisk.ReloadModule(name)
-		reply(nil, err)
-	})
+	srv.subscribe("ari.asterisk.reload.>",
+		NamedHandler(len("ari.asterisk.reload."), func(name string, _ []byte, reply Reply) {
+			err := srv.upstream.Asterisk.ReloadModule(name)
+			reply(nil, err)
+		}))
 
 	srv.subscribe("ari.asterisk.info", func(_ string, _ []byte, reply Reply) {
 		ai, err := srv.upstream.Asterisk.Info("")
 		reply(ai, err)
 	})
 
-	srv.subscribe("ari.asterisk.variables.get.>", func(subj string, _ []byte, reply Reply) {
-		name := subj[len("ari.asterisk.variables.get."):]
-		val, err := srv.upstream.Asterisk.Variables().Get(name)
-		reply(val, err)
-	})
+	srv.subscribe("ari.asterisk.variables.get.>",
+		NamedHandler(len("ari.asterisk.variables.get."), func(name string, _ []byte, reply Reply) {
+			val, err := srv.upstream.Asterisk.Variables().Get(name)
+			reply(val, err)
+		}))
 
-	srv.subscribe("ari.asterisk.variables.set.>", func(subj string, data []byte, reply Reply) {
-		name := subj[len("ari.asterisk.variables.set."):]
+	srv.subscribe("ari.asterisk.variables.set.>",
+		NamedHandler(len("ari.asterisk.variables.set."), func(name string, data []byte, reply Reply) {
+			var value string
+			if err := json.Unmarshal(data, &value); err != nil {
+				reply(nil, err)
+				return
+			}
 
-		var value string
-		if err := json.Unmarshal(data, &value); err != nil {
+			err := srv.upstream.Asterisk.Variables().Set(name, value)
 			reply(nil, err)
-			return
-		}
-
-		err := srv.upstream.Asterisk.Variables().Set(name, value)
-		reply(nil, err)
-	})
+		}))
 
 }
