4a0041a58ecd198e6b8dc2b02db5206d46f0c30d
Sheena Artrip <sheena.artrip@gmail.com>
2016-09-23 01:16:31 -0400

    v3 - add TextMessageData Fixes #49

--- textMessage.go
+++ textMessage.go
@@ -9,3 +9,17 @@ type TextMessage interface {
 	// SendByURI sends a text message to an endpoint by free-form URI
 	SendByURI(from, to, body string, vars map[string]string) error
 }
+
+// TextMessageData describes text message
+type TextMessageData struct {
+	Body      string                `json:"body"` // The body (text) of the message
+	From      string                `json:"from"` // Technology-specific source URI
+	To        string                `json:"to"`   // Technology-specific destination URI
+	Variables []TextMessageVariable `json:"variables,omitempty"`
+}
+
+// TextMessageVariable describes a key-value pair (associated with a text message)
+type TextMessageVariable struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
