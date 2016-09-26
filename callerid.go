a44d8dee883a1700f2d0de44501fbe9386d1d57e
Seán C McCord <ulexus@gmail.com>
2016-09-26 18:16:44 -0400

    Stub CallerIDFromString

--- callerid.go
+++ callerid.go
@@ -1,5 +1,7 @@
 package ari
 
+import "errors"
+
 //NOTE: Direct translation from ARI client 2.0
 
 // CallerID describes the name and number which
@@ -7,6 +9,16 @@ package ari
 type CallerID struct {
 	Name   string `json:"name"`
 	Number string `json:"number"`
+}
+
+// CallerIDFromString interprets the provided string
+// as a CallerID.  Usually, this string will be of the following forms:
+//   - "Name" <number>
+//   - <number>
+//   - "Name" number
+func CallerIDFromString(src string) (*CallerID, error) {
+	//TODO: implement complete callerid parser
+	return nil, errors.New("CallerIDFromString not yet implemented")
 }
 
 // String returns the stringified callerid
