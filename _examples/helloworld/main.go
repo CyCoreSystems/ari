05b2437c88b5c08584e8c3477bd653c89d627b48
Seán C McCord <ulexus@gmail.com>
2017-04-30 12:05:18 -0400

    clean up helloworld

--- _examples/helloworld/main.go
+++ _examples/helloworld/main.go
@@ -1,54 +1,39 @@
 package main
 
 import (
-	"context"
-	"os"
-
 	log15 "gopkg.in/inconshreveable/log15.v2"
 
-	"github.com/CyCoreSystems/ari"
 	"github.com/CyCoreSystems/ari/client/native"
 )
 
 func main() {
-	if i := run(); i != 0 {
-		os.Exit(i)
-	}
-}
-
-func run() int {
-
-	// setup logging
-	native.Logger = log15.New()
+	// OPTIONAL: setup logging
 	log := log15.New()
+	native.Logger = log
 
-	opts := native.Options{
+	log.Info("Connecting")
+
+	cl, err := native.Connect(&native.Options{
 		Application:  "example",
 		Username:     "admin",
 		Password:     "admin",
 		URL:          "http://localhost:8088/ari",
 		WebsocketURL: "ws://localhost:8088/ari/events",
-	}
-
-	log.Info("Connecting")
-
-	cl, err := native.Connect(context.TODO(), &opts)
+	})
 	if err != nil {
 		log.Error("Failed to build native ARI client", "error", err)
-		return -1
+		return
 	}
 
 	defer cl.Close()
 
 	log.Info("Connected")
 
-	info, err := cl.Asterisk().Info(&ari.Key{Kind: "build"})
+	info, err := cl.Asterisk().Info(nil)
 	if err != nil {
 		log.Error("Failed to get Asterisk Info", "error", err)
-		return -1
+		return
 	}
 
 	log.Info("Asterisk Info", "info", info)
-
-	return 0
 }
