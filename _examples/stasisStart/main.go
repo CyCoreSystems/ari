24fedf9f42b757b20d5dc376e2b572c5834e15e9
Seán C McCord <ulexus@gmail.com>
2017-04-30 20:08:51 -0400

    update stasisStart example

--- _examples/stasisStart/main.go
+++ _examples/stasisStart/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"net/http"
-	"os"
 	"sync"
-	"time"
 
 	"golang.org/x/net/context"
 
@@ -17,15 +15,91 @@ import (
 var log = log15.New()
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	<-time.After(5 * time.Second)
+	// connect
 
-	if i := run(); i != 0 {
-		os.Exit(i)
+	cl, err := native.Connect(&native.Options{
+		Application:  "example",
+		Username:     "admin",
+		Password:     "admin",
+		URL:          "http://asterisk:8088/ari",
+		WebsocketURL: "ws://asterisk:8088/ari/events",
+	})
+	if err != nil {
+		log.Error("Failed to build nc ARI client", "error", err)
+		return
 	}
+
+	// setup app
+
+	log.Info("Starting listener app")
+
+	go listenApp(ctx, cl, channelHandler)
+
+	// start call start listener
+
+	log.Info("Starting HTTP Handler")
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// make call
+		log.Info("Make sample call")
+		h, err := createCall(cl)
+		if err != nil {
+			log.Error("Failed to create call", "error", err)
+			w.WriteHeader(http.StatusBadGateway)
+			w.Write([]byte("Failed to create call: " + err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(h.ID()))
+	}))
+
+	log.Info("Listening for requests on port 9990")
+	http.ListenAndServe(":9990", nil)
+
+	return
 }
 
-func channelHandler(cl *ari.Client, h *ari.ChannelHandle) {
+func listenApp(ctx context.Context, cl ari.Client, handler func(cl ari.Client, h *ari.ChannelHandle)) {
+	sub := cl.Bus().Subscribe(nil, "StasisStart")
+	end := cl.Bus().Subscribe(nil, "StasisEnd")
+
+	for {
+		select {
+		case e := <-sub.Events():
+			log.Info("Got stasis start")
+			v := e.(*ari.StasisStart)
+			go handler(cl, cl.Channel().Get(v.Key(ari.ChannelKey, v.Channel.ID)))
+		case <-end.Events():
+			log.Info("Got stasis end")
+		case <-ctx.Done():
+			return
+		}
+	}
+
+}
+
+func createCall(cl ari.Client) (h *ari.ChannelHandle, err error) {
+	h, err = cl.Channel().Create(nil, ari.ChannelCreateRequest{
+		Endpoint: "Local/1000",
+		App:      "example",
+	})
+
+	return
+}
+
+func connect(ctx context.Context) (cl ari.Client, err error) {
+
+	log.Info("Connecting")
+
+	cl, err = native.Connect(&native.Options{})
+	return
+}
+
+func channelHandler(cl ari.Client, h *ari.ChannelHandle) {
 	log.Info("Running channel handler")
 
 	stateChange := h.Subscribe(ari.Events.ChannelStateChange)
@@ -72,93 +146,4 @@ func channelHandler(cl *ari.Client, h *ari.ChannelHandle) {
 	wg.Wait()
 
 	h.Hangup()
-}
-
-func run() int {
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// connect
-
-	cl, err := connect(ctx)
-	if err != nil {
-		log.Error("Failed to build nc ARI client", "error", err)
-		return -1
-	}
-
-	// setup app
-
-	log.Info("Starting listener app")
-
-	go listenApp(ctx, cl, channelHandler)
-
-	// start call start listener
-
-	log.Info("Starting HTTP Handler")
-
-	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		// make call
-		log.Info("Make sample call")
-		h, err := createCall(cl)
-		if err != nil {
-			log.Error("Failed to create call", "error", err)
-			w.WriteHeader(500)
-			w.Write([]byte("Failed to create call: " + err.Error()))
-			return
-		}
-
-		w.WriteHeader(200)
-		w.Write([]byte(h.ID()))
-	}))
-
-	log.Info("Listening for requests on port 9990")
-	http.ListenAndServe(":9990", nil)
-
-	return 0
-}
-
-func listenApp(ctx context.Context, cl *ari.Client, handler func(cl *ari.Client, h *ari.ChannelHandle)) {
-	sub := cl.Bus.Subscribe("StasisStart")
-	end := cl.Bus.Subscribe("StasisEnd")
-
-	for {
-		select {
-		case e := <-sub.Events():
-			log.Info("Got stasis start")
-			stasisStartEvent := e.(*ari.StasisStart)
-			go handler(cl, cl.Channel.Get(stasisStartEvent.Channel.ID))
-		case <-end.Events():
-			log.Info("Got stasis end")
-		case <-ctx.Done():
-			return
-		}
-	}
-
-}
-
-func createCall(cl *ari.Client) (h *ari.ChannelHandle, err error) {
-	h, err = cl.Channel.Create(ari.ChannelCreateRequest{
-		Endpoint: "Local/1000",
-		App:      "example",
-	})
-
-	return
-}
-
-func connect(ctx context.Context) (cl *ari.Client, err error) {
-
-	opts := native.Options{
-		Application:  "example",
-		Username:     "admin",
-		Password:     "admin",
-		URL:          "http://asterisk:8088/ari",
-		WebsocketURL: "ws://asterisk:8088/ari/events",
-		Context:      ctx,
-	}
-
-	log.Info("Connecting")
-
-	cl, err = native.New(opts)
-	return
 }
