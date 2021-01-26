f836fb1ff2d0b10f00f973d5f587d8a8c29fcb1f
Vitor Espindola <vitor.espindola@byne.com.br>
2021-01-26 15:06:11 -0300

    Add support for ARI redirect method

--- channel.go
+++ channel.go
@@ -44,6 +44,9 @@ type Channel interface {
 	// Continue tells Asterisk to return a channel to the dialplan
 	Continue(key *Key, context, extension string, priority int) error
 
+	// Redirect tells Asterisk to redirect the channel to a different location
+	Redirect(key *Key, endpoint string) error
+
 	// Busy hangs up the channel with the "busy" cause code
 	Busy(key *Key) error
 
@@ -318,6 +321,11 @@ func (ch *ChannelHandle) Data() (*ChannelData, error) {
 // Continue tells Asterisk to return the channel to the dialplan
 func (ch *ChannelHandle) Continue(context, extension string, priority int) error {
 	return ch.c.Continue(ch.key, context, extension, priority)
+}
+
+// Redirect tells Asterisk to redirect the channel to a different location
+func (ch *ChannelHandle) Redirect(endpoint string) error {
+	return ch.c.Redirect(ch.key, endpoint)
 }
 
 //---
