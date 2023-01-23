cf7650640988aca03a5eaf5ee0a09df5eaccd759
serfreeman1337 <serfreeman1337@gmail.com>
2023-01-23 16:02:29 +0300

    allow multiple media URIs for playback (#142)

--- client/arimocks/Player.go
+++ client/arimocks/Player.go
@@ -13,12 +13,19 @@ type Player struct {
 }
 
 // Play provides a mock function with given fields: _a0, _a1
-func (_m *Player) Play(_a0 string, _a1 string) (*ari.PlaybackHandle, error) {
-	ret := _m.Called(_a0, _a1)
+func (_m *Player) Play(_a0 string, _a1 ...string) (*ari.PlaybackHandle, error) {
+	_va := make([]interface{}, len(_a1))
+	for _i := range _a1 {
+		_va[_i] = _a1[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, _a0)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
 
 	var r0 *ari.PlaybackHandle
-	if rf, ok := ret.Get(0).(func(string, string) *ari.PlaybackHandle); ok {
-		r0 = rf(_a0, _a1)
+	if rf, ok := ret.Get(0).(func(string, ...string) *ari.PlaybackHandle); ok {
+		r0 = rf(_a0, _a1...)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*ari.PlaybackHandle)
@@ -26,8 +33,8 @@ func (_m *Player) Play(_a0 string, _a1 string) (*ari.PlaybackHandle, error) {
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(string, string) error); ok {
-		r1 = rf(_a0, _a1)
+	if rf, ok := ret.Get(1).(func(string, ...string) error); ok {
+		r1 = rf(_a0, _a1...)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -36,12 +43,19 @@ func (_m *Player) Play(_a0 string, _a1 string) (*ari.PlaybackHandle, error) {
 }
 
 // StagePlay provides a mock function with given fields: _a0, _a1
-func (_m *Player) StagePlay(_a0 string, _a1 string) (*ari.PlaybackHandle, error) {
-	ret := _m.Called(_a0, _a1)
+func (_m *Player) StagePlay(_a0 string, _a1 ...string) (*ari.PlaybackHandle, error) {
+	_va := make([]interface{}, len(_a1))
+	for _i := range _a1 {
+		_va[_i] = _a1[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, _a0)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
 
 	var r0 *ari.PlaybackHandle
-	if rf, ok := ret.Get(0).(func(string, string) *ari.PlaybackHandle); ok {
-		r0 = rf(_a0, _a1)
+	if rf, ok := ret.Get(0).(func(string, ...string) *ari.PlaybackHandle); ok {
+		r0 = rf(_a0, _a1...)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*ari.PlaybackHandle)
@@ -49,8 +63,8 @@ func (_m *Player) StagePlay(_a0 string, _a1 string) (*ari.PlaybackHandle, error)
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(string, string) error); ok {
-		r1 = rf(_a0, _a1)
+	if rf, ok := ret.Get(1).(func(string, ...string) error); ok {
+		r1 = rf(_a0, _a1...)
 	} else {
 		r1 = ret.Error(1)
 	}
